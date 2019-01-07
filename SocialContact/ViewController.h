125dd223ebc8d24948b414d1c9b42b43835d6e8e
EDZ <1012256310@qq.com>
2019-01-07 20:38:07 +0800

    Initial Commit

---
+++ SocialContact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialContact
+//
+//  Created by EDZ on 2019/1/7.
+//  Copyright © 2019 ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SocialContact/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SocialContact
+//
+//  Created by EDZ on 2019/1/7.
+//  Copyright © 2019 ha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
